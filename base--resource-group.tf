c44b699221953db00f6117e1d0f682449dc6e5a9
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 17:35:13 +0800

    Test pipeline

--- base--resource-group.tf
+++ base--resource-group.tf
@@ -1,17 +1,17 @@
 module "resource_group" {
-  source = "git::https://github.com/David97/azure-terraform-module-monorepo.git//base/resource-group?ref=v1.0.4"
+  source = "git@github.com:David97/azure-terraform-module-monorepo.git//base/resource-group?ref=v1.0.4"
 
   resource_groups = [
     {
       location = "eastasia"
-      name     = "test-henry"
+      name     = "test-henry-1"
       tags = {
         "Name" = "Test Henry"
       }
     },
     {
       location = "eastasia"
-      name     = "test-swo"
+      name     = "test-swo-1"
       tags = {
         "Name" = "Test SWO"
       }
