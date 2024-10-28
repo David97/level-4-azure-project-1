13ef4d04a1a9762c9a5f76290d8946a0e28e7f66
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 17:56:10 +0800

    Update pipeline

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 terraform {
-  backend "azurerm" {
+  backend "remote" {
     resource_group_name  = "hkjc-landing-zone-tf-state"
     storage_account_name = "hkjclandingzonetfstate"
     container_name       = "hkjc-landing-zone-tf-state"
     key                  = "non-prod/terraform.tfstate"
-  }
 
-  backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "SoftwareOne"
 
