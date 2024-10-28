de1e3d2e12ec3afb0495d418dd1b8ec36b82dc1a
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 18:36:40 +0800

    Update provider

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.52.0"
+    }
+  }
+
   backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "SoftwareOne"
