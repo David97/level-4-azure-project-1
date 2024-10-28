42f9beb0cc8856d7ad89ab259474386eb67947cb
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 17:26:41 +0800

    Added GitHub Actions

--- main.tf
+++ main.tf
@@ -4,5 +4,13 @@ terraform {
     storage_account_name = "hkjclandingzonetfstate"
     container_name       = "hkjc-landing-zone-tf-state"
     key                  = "non-prod/terraform.tfstate"
+
+    # The name of your Terraform Cloud organization.
+    organization = "SoftwareOne"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "azure-project-1"
+    }
   }
 }
