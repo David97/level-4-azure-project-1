1a0a8ebd7b7110c40a5b2c583715995e23871b23
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 18:41:36 +0800

    Update pipeline

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.102.0"
-    }
-  }
-
   backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "SoftwareOne"
