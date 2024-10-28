9d0266ea51c3f43a038e93d9054be55166ff4cc2
Henry Cheung <henry.cheung@softwareone.com>
2024-10-28 18:06:44 +0800

    Update pipeline

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ terraform {
     storage_account_name = "hkjclandingzonetfstate"
     container_name       = "hkjc-landing-zone-tf-state"
     key                  = "non-prod/terraform.tfstate"
+  }
 
-    backend "remote" {
-      # The name of your Terraform Cloud organization.
-      organization = "SoftwareOne"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "SoftwareOne"
 
-      # The name of the Terraform Cloud workspace to store Terraform state files in.
-      workspaces {
-        name = "azure-project-1"
-      }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "azure-project-1"
     }
   }
 }
