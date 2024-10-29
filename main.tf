e1135f62663faebcf52ff8b51745faeded3ff034
Henry Cheung <henry.cheung@softwareone.com>
2024-10-29 10:24:50 +0800

    Test pipeline

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
 terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+  }
+
   cloud {
     # The name of your Terraform Cloud organization.
     organization = "SoftwareOne"
@@ -6,12 +27,6 @@ terraform {
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
       name = "azure-project-1"
-    }
-  }
-
-  required_providers {
-    azapi = {
-      source = "azure/azapi"
     }
   }
 }
